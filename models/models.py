a65158cb01e9fb229b42c0d691eb693835eb6827
Waseem Ahmad <waseem@rice.edu>
2013-02-09 20:44:27 +0000

    Wrote course definitions.

--- models/models.py
+++ models/models.py
@@ -3,6 +3,7 @@ Database storage for the app.
 """
 
 from google.appengine.ext import db
+from google.appengine.ext.db import polymodel
 
 class User(db.Model):
     net_id = db.StringProperty(required=True)
@@ -13,12 +14,20 @@ def get_user(net_id, create=False):
         user = User(net_id=net_id).put()
     return user
 
-class Sticky(db.Model):
-    """
-    To be deleted soon.
-    """
-    user = db.ReferenceProperty(User,
-                                required=True)
-    time_added = db.DateTimeProperty(auto_now=True)
-    title = db.StringProperty(required=True)
-    note = db.TextProperty()
+class Course(db.Model):
+    subject = db.ReferenceProperty(Subject, required=True)
+    number = db.StringProperty(required=True)     # E.g. 182
+    term = db.ListProperty(db.Key)      # List of terms its been taught
+    description = db.TextProperty()
+    xlink = db.StringProperty()
+    credit_hours = db.IntegerProperty(required=True)
+
+class Term(db.Model):
+    code = db.StringProperty(required=True) # E.g. 201320
+    description = db.StringProperty()       # E.g. Spring 2013
+
+class Department(db.Model):
+    name = db.StringProperty(required=True)
+
+class Subject(db.Model):
+    code = db.StringProperty(required=True)     # E.g. COMP
