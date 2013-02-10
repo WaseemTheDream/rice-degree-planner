c4a3c069e8a21103b54cf3782d25fc41f80e5f57
WenXing <wen.xing@rice.edu>
2013-02-10 11:36:48 -0600

    adjust check icon

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ class MainHandler(webapp2.RequestHandler):
         template = JINJA_ENV.get_template('/views/%s.html' % page)
         page_data = {}
         page_data['net_id'] = user.net_id
-<<<<<<< HEAD
+
         page_data['terms'] = []
         requiredTerm = models.Term.gql('WHERE code=:1', '201320').get()
         #if requiredTerm.key() not in user.terms:
@@ -55,13 +55,6 @@ class MainHandler(webapp2.RequestHandler):
         
         sorted_terms = sorted(models.Term.get(user.terms), key=lambda t:t.code)
         for term in sorted_terms:
-=======
-
-        page_data['terms'] = []     
-
-        terms = models.Term.gql('LIMIT 10')
-        for term in terms:
->>>>>>> css and python comment
             thisTerm = {
                 'code': term.code,
                 'description':term.description
@@ -155,8 +148,3 @@ class DeleteCourseHandler(webapp2.RequestHandler):
         course_taken.delete()
         self.response.out.write('Deleted')
         
-app = webapp2.WSGIApplication([
-    ('/addCourse', AddCourseHandler),
-    ('/deleteCourse', DeleteCourseHandler),
-    ('/.*', MainHandler)
-], debug=True)
