93db1b16f785eb2c50db19670222fd0967c75509
Joshua Lipschultz <josh.lipschultz@gmail.com>
2013-02-09 20:29:01 -0600

    working on a post function in main.py

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ import json
 import logging
 import webapp2
 
+from models import models
 from google.appengine.ext import db
 from authentication import auth
 from authentication.gaesessions import get_current_session
@@ -41,6 +42,40 @@ class MainHandler(webapp2.RequestHandler):
         template = JINJA_ENV.get_template('/views/%s.html' % page)
         self.response.out.write(template.render())
 
+class AddCourseHandler(webapp2.RequestHandler):
+    def post(self):
+        session get_current_session()
+        
+        # Uncomment when we have the login page working
+        # if not session.has_key('net_id'):
+        #     return
+
+        user = get_user(session['net_id'])
+        data = jason.loads(self.request.get('json'))
+        logging.info(data)
+
+        course = data['course'].split()
+        subject_entity = models.Subject('WHERE code=:1',)
+        course_entity = models.Course('WHERE code=:1',201310)
+
+        coursetaken = CourseTaken(user = user.key(),
+                                  course = ,
+                                  term = models.Term('WHERE code=:1',201310))
+
+        # coursetaken = CourseTaken(user = user.key(),
+        #                           course = data['course'],
+        #                           term = data['term'])
+        coursetaken.put()
+        data['id'] = str(sticky.key())
+        self.response.out.write(json.dumps(data))
+
+        # {"term":"{term_id}","course":"COMP 140"}
+
+        
+
+        
+
 app = webapp2.WSGIApplication([
     ('/.*', MainHandler)
+    ('/addcourse', AddCourseHandler)
 ], debug=True)
