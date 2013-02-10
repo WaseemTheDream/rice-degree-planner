6c1ca5318c15d418a9a9da639d3b18222e824ede
Caleb Solano <caso.grande@gmail.com>
2013-02-09 18:02:56 -0600

    Changes "Add Class" UI

---
+++ static/js/degree-planner.js
@@ -0,0 +1,30 @@
+$(function() {
+	$(".classes").append('<button class="add-class-button">Add Class</button>');
+	$(".add-class-button").click(function(){
+		openAddClassPopup();
+	});
+});
+
+function openAddClassPopup(){
+	$html = '<div id="add-class-popup"><span class="popup-title">Enter Class Name</span><div id="search-container"><input type="text" name="search" id="search" /><input type="hidden" name="class_id" id="class_id" /><div class="note">Ex: COMP 140, PHIL 103, CAAM 335...</div></div><button id="submit-add-class" class="button">Add Class</button></div>';
+	$.fancybox($html);
+	$("#submit-add-class").click(function(){
+		submitAddClass();
+	});
+	$("#search").autocomplete({
+		source:"GetClasses.html",
+		select:function(event, ui){
+			$("#class_id").val(ui.item.class_id);
+		}
+	});
+}
+
+function submitAddClass(){
+	if(!$("#class_id").val()){
+		$("#search").css("border-color","#F33");
+		$("#search-container").effect("shake", { times:2, distance:5 }, 500);
+	}else{
+		alert("I should be telling the server that you want to add course \""+$("#class_id").val()+"\"");
+		$.fancybox.close();
+	}
+}
