91947b31f74c823dff9d0d44d794ec8a1f8ddd64
Waseem Ahmad <waseem@rice.edu>
2013-02-10 18:02:02 +0000

    Implemented rudimentary comp sci requirements need to test.

--- setup.py
+++ setup.py
@@ -19,13 +19,15 @@ def main():
     course_processor.process_xml('courses_data/fall12.xml')
     course_processor.process_xml('courses_data/spring13.xml')
 
-def add_comp_sci_major():
+def comp_sci_math_and_science_group():
+    # Compute Math and Science Requirements
     calculus = [models.get_course(course) for course in ['MATH 101', 'MATH 102']]
     assert (None not in calculus)
-    culculus_requirement = models.CoursesRequirement(
+    calculus_requirement = models.CoursesRequirement(
         name='Introductory calculus',
         options=calculus,
         num_required=2)
+    calculus_requirement.put()
 
     advanced_calculus = [models.get_course(course) for course in ['MATH 211', 'MATH 212', 'MATH 221', 'MATH 222']]
     assert (None not in advanced_calculus)
@@ -33,6 +35,49 @@ def add_comp_sci_major():
         name='Advanced calculus',
         options=advanced_calculus,
         num_required=1)
+    advanced_calculus_requirement.put()
+
+    math_and_science_group = models.RequirementGroup(name='Math and Science')
+    for req in [calculus_requirement, advanced_calculus_requirement]:
+        math_and_science_group.requirements.append(req.key())
+    math_and_science_group.put()
+
+    # TODO: Finish this
+
+    return math_and_science_group
+
+def comp_sci_core_group():
+    # Compute CS Core requirements
+    intro = [models.get_course(course) for course in ['COMP 140', 'COMP 160']]
+    assert (None not in intro)
+    intro_requirement = models.CoursesRequirement(
+        name='Introductory CS',
+        options=intro,
+        num_required=1)
+    intro_requirement.put()
+
+    algorithms = [models.get_course('COMP 182')]
+    assert (None not in algorithms)
+    algorithms_requirement = models.CoursesRequirement(
+        name='Algorithms',
+        options=algorithms,
+        num_required=1)
+    algorithms_requirement.put()
+
+    core_group = models.RequirementGroup(name='CS Core')
+    for req in [intro_requirement, algorithms_requirement]:
+        core_group.requirements.append(req.key())
+    core_group.put()
+    return core_group
+
+def comp_sci_major():
+    math_and_science_group = comp_sci_math_and_science_group()
+    core_group = comp_sci_core_group()
+
+    major_requirement = models.DegreeRequirement(name='Computer Science')
+    for grp in [math_and_science_group, core_group]:
+        major_requirement.requirement_groups.append(grp.key())
+    major_requirement.put()
 
 if __name__ == '__main__':
     main()
